996715f7ecb8cb53cf243faa2ddfbc11e71e168c
TAC <tac@tac42.net>
2020-05-06 17:38:29 +0900

    Fix lint

--- functions/index.js
+++ functions/index.js
@@ -2,11 +2,12 @@ const functions = require("firebase-functions");
 
 const BaseURL = "https://ja.wikipedia.org";
 const KusatsuListURL =
-  BaseURL + "/wiki/%E7%89%B9%E5%88%A5:%E5%89%8D%E6%96%B9%E4%B8%80%E8%87%B4%E3%83%9A%E3%83%BC%E3%82%B8%E4%B8%80%E8%A6%A7/%E8%8D%89%E6%B4%A5";
+  BaseURL +
+  "/wiki/%E7%89%B9%E5%88%A5:%E5%89%8D%E6%96%B9%E4%B8%80%E8%87%B4%E3%83%9A%E3%83%BC%E3%82%B8%E4%B8%80%E8%A6%A7/%E8%8D%89%E6%B4%A5";
 const axios = require("axios").default;
 const { JSDOM } = require("jsdom");
 
-let parseList = (body) => {
+let parseList = body => {
   let document = new JSDOM(body);
   let query = "#mw-content-text > div.mw-prefixindex-body > ul > li a";
   let elements = document.window.document.querySelectorAll(query);
@@ -14,13 +15,13 @@ let parseList = (body) => {
   for (let e of elements) {
     array.push({
       title: e.textContent,
-      link: e["href"],
+      link: e["href"]
     });
   }
   return array;
 };
 
-let parseDetail = (body) => {
+let parseDetail = body => {
   let document = new JSDOM(body).window.document;
   let detail = {
     title: document.querySelector("#firstHeading").textContent,
