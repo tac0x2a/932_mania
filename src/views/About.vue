9b78921615045a029adac9a2483a563fec4512dc
TAC <tac@tac42.net>
2020-05-07 00:38:16 +0900

    Fix icon siize

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,16 +3,17 @@
     <b-jumbotron header-level="5" header="932mania" lead="Welcome to world of Kusatsu...">
       <a href="https://github.com/tac0x2a" target="_blank">
         <b-avatar
-          size="4rem"
+          size="6rem"
           class="mr-4"
-          variant="success"
+          variant="secondary"
+          rounded="lg"
           src="https://avatars0.githubusercontent.com/u/245998?s=460&u=63bc2262bc711e327e55ad10a6eeafba195a644b&v=4"
         ></b-avatar>
       </a>
       <p>This is my first application by Vue.js</p>
 
       <b-button
-        variant="success"
+        variant="secondary"
         href="https://github.com/tac0x2a/932_mania"
         target="_blank"
       >View on Github</b-button>
