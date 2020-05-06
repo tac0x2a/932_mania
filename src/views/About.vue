a119aff064f7d3d4588b13856d291fc74561b3ef
TAC <tac@tac42.net>
2020-05-06 17:08:53 +0900

    Update About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>Welcome to world of Kusatsu</h1>
-    This app is sample of Vue.js
+    <b-jumbotron header-level="5" header="932mania IIDX" lead="Welcome to world of Kusatsu...">
+      <p>This is my first application of Vue.js</p>
+
+      <b-button
+        variant="primary"
+        href="https://github.com/tac0x2a/932_mania"
+        target="_blank"
+      >View on Github</b-button>
+    </b-jumbotron>
   </div>
 </template>
